e1e6b2501561872fa2480d6c212bc125a7f83a1d
Connor Holland <connorholland@Connors-MacBook-Pro.local>
2020-07-08 17:10:37 -0600

    View controllers

---
+++ GitGudGameFiles/GitGudGame/Controllers/GameDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameDetailViewController.swift
+//  GitGudGame
+//
+//  Created by Connor Holland on 7/8/20.
+//  Copyright © 2020 Hunter Smith. All rights reserved.
+//
+
+import UIKit
+
+class GameDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
