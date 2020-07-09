c8501281643114e07cffad508234b84fdcde65fe
Connor Holland <connorholland@Connors-MacBook-Pro.local>
2020-07-09 12:22:51 -0600

    Finished Buttons

--- GitGudGameFiles/GitGudGame/Controllers/QuestionViewController.swift
+++ GitGudGameFiles/GitGudGame/Controllers/QuestionViewController.swift
@@ -18,32 +18,81 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var buttonFour: UIButton!
     @IBOutlet weak var correctAnswerLabel: UILabel!
     
+    
+    // MARK: - Properties
+    var question = QuestionControllers.shared.questionIndexNumber
+    
+    // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
         questionViews()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
     // MARK: - Actions
     
-    @IBAction func buttonOneTapped(_ sender: Any) {
+    @IBAction func buttonOneTapped(_ sender: UIButton) {
+        let correctAnswerIndex = QuestionControllers.shared.questions[question].correctAnswer
+        let answerArray = QuestionControllers.shared.questions[question].answer
+        let x = answerArray[correctAnswerIndex]
+        print(x)
+        if sender.currentTitle == x {
+            print("Correct Answer")
+        } else {
+            print("Wrong Answer")
+        }
+        
+        
     }
-    @IBAction func buttonTwoTapped(_ sender: Any) {
+    @IBAction func buttonTwoTapped(_ sender: UIButton) {
+        let correctAnswerIndex = QuestionControllers.shared.questions[question].correctAnswer
+        let answerArray = QuestionControllers.shared.questions[question].answer
+        let x = answerArray[correctAnswerIndex]
+        print(x)
+        if sender.currentTitle == x {
+            print("Correct Answer")
+        } else {
+            print("Wrong Answer")
+        }
+
+        
     }
-    @IBAction func buttonThreeTapped(_ sender: Any) {
+    @IBAction func buttonThreeTapped(_ sender: UIButton) {
+        let correctAnswerIndex = QuestionControllers.shared.questions[question].correctAnswer
+        let answerArray = QuestionControllers.shared.questions[question].answer
+        let x = answerArray[correctAnswerIndex]
+        print(x)
+        if (sender as AnyObject).currentTitle == x {
+            print("Correct Answer")
+        } else {
+            print("Wrong Answer")
+        }
     }
-    @IBAction func buttonFourTapped(_ sender: Any) {
+    @IBAction func buttonFourTapped(_ sender: UIButton) {
+        let correctAnswerIndex = QuestionControllers.shared.questions[question].correctAnswer
+        let answerArray = QuestionControllers.shared.questions[question].answer
+        let x = answerArray[correctAnswerIndex]
+        print(x)
+        if sender.currentTitle == x {
+            print("Correct Answer")
+        } else {
+            print("Wrong Answer")
+        }
     }
     
     
     
     // MARK: - Methods
     func questionViews() {
-        let currentQuestion = QuestionControllers.shared.questions[0].question
+        let currentQuestion = QuestionControllers.shared.questions[question].question
         questionLabel.text = currentQuestion
-        let answer1 = QuestionControllers.shared.questions[0].answer[0]
-        let answer2 = QuestionControllers.shared.questions[0].answer[1]
-        let answer3 = QuestionControllers.shared.questions[0].answer[2]
-        let answer4 = QuestionControllers.shared.questions[0].answer[3]
+        let answer1 = QuestionControllers.shared.questions[question].answer[0]
+        let answer2 = QuestionControllers.shared.questions[question].answer[1]
+        let answer3 = QuestionControllers.shared.questions[question].answer[2]
+        let answer4 = QuestionControllers.shared.questions[question].answer[3]
         buttonOne.setTitle(answer1, for: .normal)
         buttonTwo.setTitle(answer2, for: .normal)
         buttonThree.setTitle(answer3, for: .normal)
