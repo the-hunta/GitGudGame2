7f3df36fe532e785965929ce5dbc4a33dff3c59b
Connor Holland <connorholland@Connors-MacBook-Pro.local>
2020-07-09 11:11:08 -0600

    Built out key question view controller functions

--- GitGudGameFiles/GitGudGame/Controllers/QuestionViewController.swift
+++ GitGudGameFiles/GitGudGame/Controllers/QuestionViewController.swift
@@ -9,10 +9,50 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
-
+    
+    @IBOutlet weak var questionTextField: UITextView!
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var buttonTwo: UIButton!
+    @IBOutlet weak var buttonThree: UIButton!
+    @IBOutlet weak var buttonFour: UIButton!
+    @IBOutlet weak var correctAnswerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        questionViews()
     }
+    
+    // MARK: - Actions
+    
+    @IBAction func buttonOneTapped(_ sender: Any) {
+    }
+    @IBAction func buttonTwoTapped(_ sender: Any) {
+    }
+    @IBAction func buttonThreeTapped(_ sender: Any) {
+    }
+    @IBAction func buttonFourTapped(_ sender: Any) {
+    }
+    
+    
+    
+    // MARK: - Methods
+    func questionViews() {
+        let currentQuestion = QuestionControllers.shared.questions[0].question
+        questionTextField.text = currentQuestion
+        let answer1 = QuestionControllers.shared.questions[0].answer[0]
+        let answer2 = QuestionControllers.shared.questions[0].answer[1]
+        let answer3 = QuestionControllers.shared.questions[0].answer[2]
+        let answer4 = QuestionControllers.shared.questions[0].answer[3]
+        buttonOne.setTitle(answer1, for: .normal)
+        buttonTwo.setTitle(answer2, for: .normal)
+        buttonThree.setTitle(answer3, for: .normal)
+        buttonFour.setTitle(answer4, for: .normal)
+    }
+    
+    
+    
+    
+    
     
 
     /*
